3cf8afe88063b05b2ddbde1d3e4179e9a5a365a1
Jonathan Anz <jvanz@live.com>
2025-04-02 16:19:10 -0700

    Added Movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -19,13 +19,34 @@ public class Player : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
 
         // Reset moveDelta
-        moveDelta = new Vector3(x, y, 0f);
+        moveDelta = new Vector3(x, y, 0f).normalized;
 
         // Swap sprite direction, whether you're going right or left
         if (moveDelta.x > 0) transform.localScale = Vector3.one;
         else if (moveDelta.x < 0) transform.localScale = new Vector3(-1f, 1f, 1f);
 
-        // Move
-        transform.Translate(moveDelta.normalized * Time.fixedDeltaTime);
+        hit = Physics2D.BoxCast(transform.position, 
+                                boxCollider.size, 
+                                0f, 
+                                new Vector2(0f, moveDelta.y), 
+                                Mathf.Abs(moveDelta.y * Time.deltaTime), 
+                                LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Move
+            transform.Translate(0f, moveDelta.y * Time.fixedDeltaTime, 0f);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, 
+                                boxCollider.size, 
+                                0f, 
+                                new Vector2(moveDelta.x, 0f), 
+                                Mathf.Abs(moveDelta.x * Time.deltaTime), 
+                                LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Move
+            transform.Translate(moveDelta.x * Time.fixedDeltaTime, 0f, 0f);
+        }
     }
 }
